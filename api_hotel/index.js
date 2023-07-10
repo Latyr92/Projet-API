b96512f63be414e82af537e3eecb124c0261c5e5
Satou-Diop <aissatoudiop6@esp.sn>
2023-07-10 09:29:26 +0000

    Depot des fichiers

---
+++ api_hotel/index.js
@@ -0,0 +1,34 @@
+import hotelRoute from "./routes/hotel.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from "dotenv"
+import db from './configuration.js';
+
+
+const app = express();
+dotenv.config();
+app.use(bodyParser.json());
+
+
+
+// Vérifier si la connexion à la base de données a réussi
+db.connect((err, client, release) => {
+  if (err) {
+    return console.error('Erreur de connexion à la base de données', err);
+  }
+  console.log('Connexion à la base de données réussie');
+  client.release();
+});
+
+
+//API ENDPOINT
+app.get('/',(req,res)=>res.status(200).send("Hello World"));
+
+app.use("/api/hotel",hotelRoute);
+
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Serveur en écoute sur le port ${port}`);
+});
---
+++ api_hotel/routes/hotel.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { createHotel,updateHotel,getHotel,getByID,deleteHotel} from '../controllers/hotel.js';
+
+const router =express.Router();
+// Creer un hotel
+router.post("/", createHotel);
+
+// Recuperer les hotelsd
+router.get('/', getHotel);
+router.get('/:id', getByID);
+
+//Update un hotel
+router.put('/:id',updateHotel);
+
+//Supprimer un hotel
+router.delete("/:id",deleteHotel)
+
+
+export default router
